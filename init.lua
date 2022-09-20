893d889b3c8c139639064c98f055040b25705c98
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-20 20:02:43 +0200

    Add more colours of floppies

--- init.lua
+++ init.lua
@@ -1,32 +1,39 @@
 
-local throw = function(itemstack, user, pointed_thing)
-		itemstack:take_item()
-		local pos = user:getpos()
-		local dir = user:get_look_dir()
-		local yaw = user:get_look_yaw()
-		if pos and dir then
-			pos.y = pos.y + 1.5
-			local obj = minetest.add_entity(pos, "floppy:floppy_red")
-			if obj then
-				obj:setvelocity({x=dir.x * 25, y=dir.y * 25, z=dir.z * 25})
-				obj:setacceleration({x=dir.x * -3, y=-25, z=dir.z * -3})
-			end
-		end
-		return itemstack
-	end
+local colours = {
+	red = 'Red',
+	green = 'Green',
+	blue = 'Blue'
+}
 
-minetest.register_craftitem("floppy:floppy_red", {
-	description = "Red Floppy",
-	stack_max = 64,
-	inventory_image = "floppy_red.png",
+for colour, colourdesc in pairs(colours) do
+
+local throw = function(itemstack, user, pointed_thing)
+	itemstack:take_item()
+	local pos = user:getpos()
+	local dir = user:get_look_dir()
+	local yaw = user:get_look_yaw()
+	if pos and dir then
+		pos.y = pos.y + 1.5
+		local obj = minetest.add_entity(pos, "floppy:floppy_"..colour)
+		if obj then
+			obj:setvelocity({x=dir.x * 25, y=dir.y * 25, z=dir.z * 25})
+			obj:setacceleration({x=dir.x * -3, y=-25, z=dir.z * -3})
+		end
+	end
+	return itemstack
+end
+
+minetest.register_craftitem("floppy:floppy_"..colour, {
+	description = colourdesc.." Floppy",
+	inventory_image = "floppy_"..colour..".png",
 	on_place = throw,
 	on_secondary_use = throw
 })
 
-minetest.register_entity("floppy:floppy_red", {
+minetest.register_entity("floppy:floppy_"..colour, {
 	visual = "item",
 	visual_size = {x=1, y=1, z=1.5},
-	textures = {'floppy:floppy_red'},
+	textures = {'floppy:floppy_'..colour},
 	physical = true,
 	on_step = function(self, dtime, collision)
 		if collision.touching_ground then
@@ -34,7 +41,7 @@ minetest.register_entity("floppy:floppy_red", {
 				if coll.type == "node" and coll.axis == "y" then
 					local pos = coll.node_pos
 					pos.y = pos.y + 1
-					minetest.set_node(pos, {name="floppy:floppy_red_lying"})
+					minetest.set_node(pos, {name="floppy:floppy_"..colour.."_lying"})
 					self.object:remove()
 				end
 			end
@@ -52,9 +59,9 @@ minetest.register_entity("floppy:floppy_red", {
 	end
 })
 
-minetest.register_node("floppy:floppy_red_lying", {
-	description = "Red Floppy (Lying)",
-	tiles = { "floppy_red.png", "floppy_red.png", "floppy_side.png" },
+minetest.register_node("floppy:floppy_"..colour.."_lying", {
+	description = colourdesc.." Floppy (Lying)",
+	tiles = { "floppy_"..colour..".png", "floppy_"..colour..".png", "floppy_side.png" },
 	drawtype = "nodebox",
 	paramtype = "light",
 	node_box = {
@@ -73,18 +80,20 @@ minetest.register_node("floppy:floppy_red_lying", {
 			{-0.4375, -0.5, -0.4375, 0.4375, -0.375, 0.4375}
 		}
 	},
-	drop = "floppy:floppy_red",
+	drop = "floppy:floppy_"..colour,
 	groups = { snappy=3, cracky=3, oddly_breakable_by_hand=3, crumbly=3, not_in_creative_inventory=1 }
 })
 
 if minetest.get_modpath("default") then
+	local wool = "wool:"..colour
 	minetest.register_craft({
-		output = "floppy:floppy_red",
+		output = "floppy:floppy_"..colour,
 		recipe = {
-			{"wool:red", "default:steel_ingot", "wool:red"},
-			{"wool:red", "wool:red", "wool:red"},
-			{"wool:red", "default:paper", "wool:red"},
+			{wool, "default:steel_ingot", wool},
+			{wool, wool, wool},
+			{wool, "default:paper", wool},
 		}
 	})
 end
 
+end
